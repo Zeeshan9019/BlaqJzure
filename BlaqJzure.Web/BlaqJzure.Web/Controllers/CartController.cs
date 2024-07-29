3eb5083091034e9692fa88ccee45297739cc1d88
DESKTOP-M61RKD9\PC <zee.web.323212@gmail.com>
2024-07-29 10:37:27 -0700

    ZA: LAtest

---
+++ BlaqJzure.Web/BlaqJzure.Web/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlaqJzure.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
